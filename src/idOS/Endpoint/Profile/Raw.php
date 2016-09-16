376ec2b3cc90991c93981f8f54e0ba97ebec3f2a
Flavio Heleno <flaviohbatista@gmail.com>
2016-09-16 16:04:07 -0300

    Changes on Raw Endpoint

--- src/idOS/Endpoint/Profile/Raw.php
+++ src/idOS/Endpoint/Profile/Raw.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace idOS\Endpoint\Profile\Source;
+namespace idOS\Endpoint\Profile;
 
 use GuzzleHttp\Client;
 use idOS\Auth\AuthInterface;
 
-class Raw extends AbstractSourceEndpoint {
+class Raw extends AbstractProfileEndpoint {
     public function createNew(
+        int $sourceId,
         string $collectionName,
         array $data
     ) : array {
         return $this->sendPost(
-            sprintf('/profiles/%s/sources/%d/raw', $this->userName, $this->sourceId),
+            sprintf('/profiles/%s/raw', $this->userName),
             [],
             [
+                'source_id'  => $sourceId,
                 'collection' => $collectionName,
                 'data'       => $data
             ]
@@ -21,13 +23,15 @@ class Raw extends AbstractSourceEndpoint {
     }
 
     public function createOrUpdate(
+        int $sourceId,
         string $collectionName,
         array $data
     ) : array {
         return $this->sendPut(
-            sprintf('/profiles/%s/sources/%d/raw', $this->userName, $this->sourceId),
+            sprintf('/profiles/%s/raw', $this->userName),
             [],
             [
+                'source_id'  => $sourceId,
                 'collection' => $collectionName,
                 'data'       => $data
             ]
@@ -36,20 +40,20 @@ class Raw extends AbstractSourceEndpoint {
 
     public function listAll(array $filters = []) : array {
         return $this->sendGet(
-            sprintf('/profiles/%s/sources/%d/raw', $this->userName, $this->sourceId),
+            sprintf('/profiles/%s/raw', $this->userName),
             $filters
         );
     }
 
     public function getOne(string $collectionName) : array {
         return $this->sendGet(
-            sprintf('/profiles/%s/sources/%d/raw/%s', $this->userName, $this->sourceId, $collectionName)
+            sprintf('/profiles/%s/raw/%s', $this->userName, $collectionName)
         );
     }
 
     public function updateOne(string $collectionName, array $data) : array {
         return $this->sendPatch(
-            sprintf('/profiles/%s/sources/%d/raw/%s', $this->userName, $this->sourceId, $collectionName),
+            sprintf('/profiles/%s/raw/%s', $this->userName, $collectionName),
             [],
             [
                 'data' => $data
@@ -59,13 +63,13 @@ class Raw extends AbstractSourceEndpoint {
 
     public function deleteOne(string $collectionName) : array {
         return $this->sendDelete(
-            sprintf('/profiles/%s/sources/%d/raw/%s', $this->userName, $this->sourceId, $collectionName)
+            sprintf('/profiles/%s/raw/%s', $this->userName, $collectionName)
         );
     }
 
     public function deleteAll(array $filters = []) : array {
         return $this->sendDelete(
-            sprintf('/profiles/%s/sources/%d/raw', $this->userName, $this->sourceId),
+            sprintf('/profiles/%s/raw', $this->userName),
             $filters
         );
     }
