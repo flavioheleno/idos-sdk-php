63bbe8978f069a36fa99f8e0b312669e77b6bf53
Rafael Andreatta <eu@rafaa.me>
2016-10-03 17:30:17 -0300

    Updated functional tests for profile features endpoint

--- test/Functional/FeaturesTest.php
+++ test/Functional/FeaturesTest.php
@@ -7,6 +7,35 @@ class FeaturesTest extends AbstractFunctional {
 		parent::setUp();
 	}
 
+	public function testListAll() {
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->deleteAll();
+
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->createNew(1321189817, 'name-test-1', 'value-test-1', 'string');
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->createNew(1321189817, 'name-test-2', 3, 'integer');
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->listAll();
+
+		foreach ($response['data'] as $feature) {
+			if ($feature['name'] === 'name-test-1') {
+				$this->assertSame('value-test-1-changed', $feature['value']);
+				$this->assertSame('string', $feature['type']);
+			}
+
+			if ($feature['name'] === 'name-test-2') {
+				$this->assertSame(3, $feature['value']);
+				$this->assertSame('integer', $feature['type']);
+			}
+		}
+	}
+
 	public function testCreateNew() {
 		$this->sdk
 		    ->Profile($this->credentials['username'])
@@ -54,6 +83,111 @@ class FeaturesTest extends AbstractFunctional {
 		$this->assertSame('name-test', $response['data']['name']);
 		$this->assertSame('value-test', $response['data']['value']);
 		$this->assertSame('string', $response['data']['type']);
+	}
+
+	public function testUpdateOne() {
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->deleteAll();
+
+		$feature = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->createNew(1321189817, 'name-test', 'value-test', 'string');
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->updateOne($feature['data']['id'], 2, 'integer');
+
+		$this->assertTrue($response['status']);
+		$this->assertNotEmpty($response['data']);
+		$this->assertNotEmpty($response['data']['creator']);
+		$this->assertSame('facebook', $response['data']['source']);
+		$this->assertSame('name-test', $response['data']['name']);
+		$this->assertSame(2, $response['data']['value']);
+		$this->assertSame('integer', $response['data']['type']);
+	}
+
+	public function testUpsertOne() {
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->deleteAll();
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->upsertOne(1321189817, 'name-test', 'value-test', 'string');
+
+		$this->assertTrue($response['status']);
+		$this->assertNotEmpty($response['data']);
+		$this->assertNotEmpty($response['data']['creator']);
+		$this->assertSame('facebook', $response['data']['source']);
+		$this->assertSame('name-test', $response['data']['name']);
+		$this->assertSame('value-test', $response['data']['value']);
+		$this->assertSame('string', $response['data']['type']);
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->upsertOne(1321189817, 'name-test', 2, 'integer');
+
+		$this->assertTrue($response['status']);
+		$this->assertNotEmpty($response['data']);
+		$this->assertNotEmpty($response['data']['creator']);
+		$this->assertSame('facebook', $response['data']['source']);
+		$this->assertSame('name-test', $response['data']['name']);
+		$this->assertSame(2, $response['data']['value']);
+		$this->assertSame('integer', $response['data']['type']);
+	}
+
+	public function testUpsertBulk() {
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->deleteAll();
+
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->createNew(1321189817, 'name-test-1', 'value-test-1', 'string');
+		$this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->createNew(1321189817, 'name-test-2', 3, 'integer');
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->upsertBulk([
+		    ['source_id' => 1321189817,
+		    'name' => 'name-test-1',
+		    'value' => 'value-test-1-changed',
+		    'type' => 'string'],
+
+		    ['source_id' => 1321189817,
+		    'name' => 'name-test-2',
+		    'value' => 'value-test-2-changed',
+		    'type' => 'string'],
+
+		    ['source_id' => 1321189817,
+		    'name' => 'name-test-3',
+		    'value' => 'value-test-3-changed',
+		    'type' => 'string']
+		]);
+
+		$response = $this->sdk
+		    ->Profile($this->credentials['username'])
+		    ->Features->listAll();
+
+		foreach ($response['data'] as $feature) {
+			if ($feature['name'] === 'name-test-1') {
+				$this->assertSame('value-test-1-changed', $feature['value']);
+				$this->assertSame('string', $feature['type']);
+			}
+
+			if ($feature['name'] === 'name-test-2') {
+				$this->assertSame('value-test-2-changed', $feature['value']);
+				$this->assertSame('string', $feature['type']);
+			}
+
+			if ($feature['name'] === 'name-test-3') {
+				$this->assertSame('value-test-3-changed', $feature['value']);
+				$this->assertSame('string', $feature['type']);
+			}
+		}
 	}
 
 	public function testDeleteOne() {
