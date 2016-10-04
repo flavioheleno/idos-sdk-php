8b1b18584ac686c6d3a1b3ac3a08291059d2b477
Rafael Andreatta <eu@rafaa.me>
2016-10-04 16:59:51 -0300

    Added functional tests for tasks endpoint and reorganized directory structure for the tests

--- test/Functional/Endpoint/Profile/GatesTest.php
+++ test/Functional/Endpoint/Profile/GatesTest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Test\Functional;
+namespace Test\Functional\Endpoint\Profile;
+
+use Test\Functional\AbstractFunctional;
 
 class GatesTest extends AbstractFunctional {
 	protected function setUp() {
